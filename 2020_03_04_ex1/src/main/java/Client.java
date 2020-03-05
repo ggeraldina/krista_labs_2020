41db9f341f6fd3216045b299bb5eae672bb440ec
Грушевская Дарья Владимировна <grushevskaya.d.v@yandex.ru>
2020-03-05 13:06:17 +0300

    Added ex2 04/03/2020

--- 2020_03_04_ex1/src/main/java/Client.java
+++ 2020_03_04_ex1/src/main/java/Client.java
@@ -1,43 +1,55 @@
 import java.io.*;
 import java.net.Socket;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.io.IOException;
+import java.io.FileOutputStream;
 
 public class Client {
-
     private static Socket clientSocket; //сокет для общения
-    private static BufferedReader reader; // нам нужен ридер читающий с консоли, иначе как
-    // мы узнаем что хочет сказать клиент?
+    private static BufferedReader reader; // нам нужен ридер читающий с консоли
     private static BufferedReader in; // поток чтения из сокета
-    private static BufferedWriter out; // поток записи в сокет
 
-
-//    https://stackoverflow.com/questions/2275443/how-to-timeout-a-thread
-    public static void main(String[] args) {
-        String ipNetwork = "192.168.175.";
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> networkElements = new ArrayList<String>();
+        String ipNetwork = "192.168.52.";
         for(int i = 0; i <= 255; i++)  {
             String ipAddress = ipNetwork + i;
+            System.out.println("Checked IP: " + ipAddress);
             try {
-                System.out.println("0");
-                clientSocket = new Socket(ipAddress, 6000); // этой строкой мы запрашиваем
-                clientSocket.setSoTimeout(100);
-                //  у сервера доступ на соединение
-                System.out.println("1");
-                reader = new BufferedReader(new InputStreamReader(System.in));
-                // читать соообщения с сервера
-                System.out.println("2");
+                clientSocket = new Socket();
+                clientSocket.connect(new InetSocketAddress(ipAddress, 6000), 10);
+                reader = new BufferedReader(new InputStreamReader(System.in)); // читать соообщения с сервера
                 in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                System.out.println("3");
-                System.out.println(in.readLine());
+                networkElements.add(ipAddress + " - " + in.readLine());
             } catch (IOException e) {
-                System.out.println("1:" + e);
+//                System.out.println("Error 1:" + e);
             } finally { // в любом случае необходимо закрыть сокет и потоки
                 try {
-                    System.out.println("Клиент был закрыт...");
-//                    clientSocket.close();
-//                    in.close();
+                    clientSocket.close();
+                    in.close();
                 } catch (Exception e2) {
-                    System.out.println("2:" + e2);
+//                    System.out.println("Error 2:" + e2);
                 }
             }
         }
+
+        writeMap(networkElements);
+    }
+
+    private static void writeMap(ArrayList<String> networkElements) throws IOException {
+        FileOutputStream fileOutputStream = new FileOutputStream("result.txt", false);
+        String result = "Network map:";
+        if (!networkElements.isEmpty()) {
+            for (String element : networkElements) {
+                result = result + "\n" + networkElements;
+            }
+        } else {
+            result += " empty";
+        }
+        fileOutputStream.write(result.getBytes());
+        fileOutputStream.close();
+
+        System.out.println(result);
     }
 }
--- 2020_03_04_ex1/src/main/java/Server.java
+++ 2020_03_04_ex1/src/main/java/Server.java
@@ -6,7 +6,6 @@ public class Server {
 
     private static Socket clientSocket; //сокет для общения
     private static ServerSocket server; // серверсокет
-    private static BufferedReader in; // поток чтения из сокета
     private static BufferedWriter out; // поток записи в сокет
 
     public static void main(String[] args) {
@@ -14,28 +13,16 @@ public class Server {
             try {
                 try {
                     server = new ServerSocket(6000); // серверсокет прослушивает порт 6000
-                    System.out.println("Сервер запущен"); // хорошо бы серверу объявить о своем запуске
+                    System.out.println("Сервер запущен"); // объявить о своем запуске
                     clientSocket = server.accept(); // accept() будет ждать пока кто-нибудь не захочет подключиться
-                    try { // установив связь и воссоздав сокет для общения с клиентом можно перейти
-                        // к созданию потоков ввода/вывода.
-                        // теперь мы можем принимать сообщения
-//                    in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                        // и отправлять
+                    try {
+                        System.out.println("!!! Новый клиент");
                         out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-
-//                    String word = in.readLine(); // ждём пока клиент что-нибудь нам напишет
-//                    System.out.println(word);
-                        System.out.println("New client");
-                        // не долго думая отвечает клиенту
-//                    out.write("HI! : " + word + "\n");
                         out.write("Grushevskaya :)");
                         out.flush(); // выталкиваем все из буфера
-
-                    } finally { // в любом случае сокет будет закрыт
-                        System.out.println("Close");
+                    } finally {
+                        System.out.println("Сокет закрыт");
                         clientSocket.close();
-                        // потоки тоже хорошо бы закрыть
-//                    in.close();
                         out.close();
                     }
                 } finally {
